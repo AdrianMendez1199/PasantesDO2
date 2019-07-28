c4d323a8af9dabcb7c9abb381d04b8a0f9bfdcbe
Adrian Mendez <mendezadrian149@gmail.com>
2019-07-28 21:31:42 +0000

    Develop

---
+++ src/models/role.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const RoleSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    unique: true,
+    maxlength: 15,
+    required: [true, 'el nombre es requerido'],
+  },
+
+  description: {
+    type: String,
+    maxlength: 150,
+  },
+
+  status: {
+    type: Boolean,
+    default: true,
+  },
+
+  create_at: {
+    type: Date,
+    default: Date.now,
+  },
+
+  update_at: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+mongoose.plugin(uniqueValidator, { message: 'el campo {PATH} es unico.' });
+
+module.exports = mongoose.model('Role', RoleSchema);
