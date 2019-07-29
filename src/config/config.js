44eca472235cc4b5045b8cbc85597312480802fb
Adrian Mendez <mendezadrian149@gmail.com>
2019-07-28 20:58:39 -0400

    url db

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,13 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-let urlDB = 'mongodb://localhost:27017/pasantesDO';
-if (process.env.NODE_ENV !== 'DEV') {
-  urlDB = 'mongodb+srv://amendez:Judoneyba1234@cluster0-ofpyn.mongodb.net/pasantesDO';
+// =============================
+// Base de datos
+// =============================
+let urlDB = 'mongodb+srv://amendez:Judoneyba1234@cluster0-ofpyn.mongodb.net/pasantesDO';
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb: //localhost:27017/pasantesDO';
 }
 
-process.env.URL_DB = urlDB;
+process.env.URLDB = urlDB;
