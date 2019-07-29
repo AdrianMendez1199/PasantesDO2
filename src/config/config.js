762c640fcfd2d0c666c75466fab3569e5352df72
Adrian Mendez <mendezadrian149@gmail.com>
2019-07-28 20:44:01 -0400

    creating enviroment var to mongoDB

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,9 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pasantesDO';
-// if (process.env.NODE_ENV !== 'DEV') {
-//     urlDB = process.env.MONGO_URI;
-// }
+let urlDB = 'mongodb://localhost:27017/pasantesDO';
+if (process.env.NODE_ENV !== 'DEV') {
+  urlDB = 'mongodb+srv://amendez:Judoneyba1234@cluster0-ofpyn.mongodb.net/pasantesDO';
+}
 
-// process.env.MONGO_URI = urlDB;
+process.env.URL_DB = urlDB;
--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require('./config');
 // const uri = 'mongodb://localhost:27017/pasantesDO';
 
 
-module.exports = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+module.exports = mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
   .then((result) => {
     console.log('conected');
   }).catch((err) => {
