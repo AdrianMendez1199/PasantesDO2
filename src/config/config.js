9723df8db8243bc0b1e45af2c05bee297a0b4e4c
Adrian Mendez <mendezadrian149@gmail.com>
2019-07-28 20:31:21 -0400

    working with auth method

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,9 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
-let urlDB = 'mongodb://localhost:27017/pasantesDO';
-if (process.env.NODE_ENV !== 'DEV') {
-    urlDB = process.env.MONGO_URI;
-}
+process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/pasantesDO';
+// if (process.env.NODE_ENV !== 'DEV') {
+//     urlDB = process.env.MONGO_URI;
+// }
 
-process.env.URL_DB = urlDB;
+// process.env.MONGO_URI = urlDB;
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,15 +1,40 @@
-const mongoose = require('mongoose');
-
+// const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 
-const auth = (req, res) => {
+const auth = async (req, res) => {
+  const { body } = req;
+  try {
+    const userLogin = await User.findOne({ email: body.email });
+
+    if (!userLogin) {
+      res.status(400).json({
+        ok: false,
+        message: '(usuario) o clave incorrecto',
+      });
+    }
+    if (!await bcrypt.compareSync(body.password, userLogin.password)) {
+      res.status(401).json({
+        ok: false,
+        message: 'usuario o (clave) incorrecto',
+      });
+    }
+
     res.status(200).json({
-        ok: true,
+      ok: true,
+      userLogin,
+      token: '123',
     });
+  } catch (err) {
+    res.status(500).json({
+      ok: false,
+      err,
+    });
+  }
 };
 
 
 module.exports = {
-    auth
-}
+  auth,
+};
